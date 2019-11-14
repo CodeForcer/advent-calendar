7337cfcf6c42f00e9c4fa299a977687e52a07f6a
codeforcer <nathancworsley@gmail.com>
2019-11-14 18:29:47 +0000

    mainnet deployment preperation

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,22 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const fs = require('fs');
+let secrets;
+if (fs.existsSync('secrets.json')) {
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+}
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(secrets.mnemonic, "https://mainnet.infura.io/v3/" + secrets.infuraApiKey);
+      },
+      network_id: "1",
     },
   },
 
